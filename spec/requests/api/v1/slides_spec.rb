0552f6480b339ddfe48d365c1b9db436e922d18d
Sal Espinosa <sespinos@gmail.com>
2016-09-29 00:01:56 -0600

    Draft first request spec

---
+++ spec/requests/api/v1/slides_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'GET /api/v1/slides' do
+  it 'returns a list of all slides' do
+    Fabricate.times(2, :slide)
+
+    get '/api/v1/slides'
+
+    expect(json_body["slides"].count).to eq(2)
+  end
+end
